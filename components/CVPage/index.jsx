46b6d269ff465178f7a83ea2ec16fa3540135996
Pavel Zapletal <wardreth@gmail.com>
2021-11-01 18:09:12 +0100

    gallery

--- components/CVPage/index.jsx
+++ components/CVPage/index.jsx
@@ -4,7 +4,6 @@ import styled from "styled-components";
 import { breakpoints, Headline, Section } from "../../styles";
 
 const CVPage = ({ educationData, experienceData }) => {
-	console.log(experienceData, "expData");
 	return (
 		<Section>
 			<MarginWrapper>
--- components/ProjectPage/index.jsx
+++ components/ProjectPage/index.jsx
@@ -2,18 +2,69 @@
 import styled from "styled-components";
 //components
 import { Headline, Section } from "../../styles";
+import { urlFor } from "../../utils/imageBuilder";
 
-const ProjectPage = () => {
+const ProjectPage = ({ projectData }) => {
+	console.log(projectData, "data");
 	return (
 		<Section colored scrollName="projects">
 			<Headline>Portfolio</Headline>
 			<Gallery>
-				<img src="/assets/2.jpg" alt="img" />
-				<img src="/assets/2.jpg" alt="img" />
+				{projectData?.map((p, i) => {
+					return (
+						<GalleryItem key={i}>
+							<img src={urlFor(p?.image[0]).url()} />
+							<div>
+								<h2>{p.headline}</h2>
+								<h1>{p.subheadline}</h1>
+							</div>
+						</GalleryItem>
+					);
+				})}
 			</Gallery>
 		</Section>
 	);
 };
+
+const GalleryItem = styled.div`
+	position: relative;
+	cursor: pointer;
+	overflow: hidden;
+
+	img {
+		width: 100%;
+		height: 100%;
+		object-fit: cover;
+	}
+
+	& > div {
+		position: absolute;
+		left: 5%;
+		top: 10%;
+		color: var(--col2);
+	}
+
+	&::before {
+		content: "";
+		position: absolute;
+		opacity: 0;
+		top: 0;
+		left: 0;
+		width: 4rem;
+		height: 4rem;
+		border-radius: 50%;
+		background-color: #1f1f1fb7;
+		transition: transform 0.5s ease-in-out;
+	}
+
+	&:hover {
+		&::before {
+			opacity: 1;
+			display: block;
+			transform: scale(40);
+		}
+	}
+`;
 
 const Gallery = styled.div`
 	display: flex;
