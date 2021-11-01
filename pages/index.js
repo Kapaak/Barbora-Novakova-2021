2945de0bae149605f8657e066efd8096f35a7e07
Pavel Zapletal <wardreth@gmail.com>
2021-11-01 16:03:49 +0100

    'fast'

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,8 @@ import ProjectPage from "../components/ProjectPage";
 import ContactPage from "../components/ContactPage";
 import { client } from "../sanity";
 
-export default function Home({ detail, projects }) {
+// export default function Home({ detail, projects }) {
+export default function Home() {
 	// const { aboutSection, educationSection, experienceSection } = detail;
 	return (
 		<>
@@ -22,37 +23,17 @@ export default function Home({ detail, projects }) {
 	);
 }
 
-export const getStaticProps = async pageContext => {
-	const query = `*[ _type == "detail"][0]{aboutSection{"file":file.asset->{url},photo,text},educationSection[]{study,location,date},experienceSection[]{company,date,job}}`;
-	const query2 = `*[_type == "project"]`;
+// export const getStaticProps = async pageContext => {
+// 	const query = `*[ _type == "detail"][0]{aboutSection{"file":file.asset->{url},photo,text},educationSection[]{study,location,date},experienceSection[]{company,date,job}}`;
+// 	const query2 = `*[_type == "project"]`;
 
-	const detail = await client.fetch(query);
-	const projects = await client.fetch(query2);
+// 	const detail = await client.fetch(query);
+// 	const projects = await client.fetch(query2);
 
-	if (!detail || !projects) return { props: null, notFound: true };
-	else
-		return {
-			props: { detail, projects },
-			revalidate: 60,
-		};
-	// const query2 = `*[ _type == "officeHour"] | order(_createdAt asc)`;
-	// const query3 = `*[ _type == "ourTeam"]{department,items[]{name,info,photo}} | order(_createdAt desc)`;
-
-	// const actualities = await client.fetch(query);
-
-	// const officeHours = await client.fetch(query2);
-
-	// const ourTeam = await client.fetch(query3);
-
-	// if (!actualities || !officeHours || !ourTeam)
-	// 	return { props: null, notFound: true };
-	// else
-	// 	return {
-	// 		props: {
-	// 			actualities,
-	// 			officeHours,
-	// 			ourTeam,
-	// 		},
-	// 		revalidate: 60,
-	// 	};
-};
+// 	if (!detail || !projects) return { props: null, notFound: true };
+// 	else
+// 		return {
+// 			props: { detail, projects },
+// 			revalidate: 60,
+// 		};
+// };
