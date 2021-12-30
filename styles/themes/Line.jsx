c80834e62e254ebf9ca3e977372bdb1f2aab630d
Pavel Zapletal <pavel.zaplet25@gmail.com>
2021-12-30 10:52:37 +0100

    fixing

--- styles/themes/Line.jsx
+++ styles/themes/Line.jsx
@@ -1,11 +1,19 @@
 //libs
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 export const Line = ({ correctWidth }) => {
-	return <StyledLine correctWidth={correctWidth} />;
+	return (
+		<StyledLine
+			initial={{ scaleX: 0 }}
+			animate={{ scaleX: 1, originX: "left" }}
+			transition={{ delay: 1, duration: 1 }}
+			correctWidth={correctWidth}
+		/>
+	);
 };
 
-const StyledLine = styled.div`
+const StyledLine = styled(motion.div)`
 	background-color: var(--col3);
 	width: ${({ correctWidth }) =>
 		correctWidth ? `calc(100% + ${correctWidth}px)` : "100%"};
