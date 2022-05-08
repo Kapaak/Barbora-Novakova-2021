129e8f9cb1a91ab601664608291a73387325ec18
kapaak <pavel.zaplet25@gmail.com>
2022-05-08 23:03:10 +0200

    meta tags

--- utils/HeadSelector.jsx
+++ utils/HeadSelector.jsx
@@ -5,6 +5,12 @@ const HeadSelector = () => {
 	return (
 		<Head>
 			<title>Barbora Novakova | portfolio</title>
+			<meta name="author" content="Barbora Novakova" />
+			<meta name="keywords" content="Barbora Novakova, portfolio" />
+			<meta
+				name="description"
+				content="Ahoj, jmenuji se Barbora Nováková a toto je mé portfolio. Jsem studentkou architektury a baví mě nejvíce veřejný prostor, urbanismus, obnova a rekonstrukce památek. Dále se zajímám o grafický design a webový design."
+			/>
 			<link rel="preconnect" href="https://fonts.googleapis.com" />
 			<link
 				rel="preconnect"
