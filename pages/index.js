736f196dd80c67f78a197f77db6e2ea63d3ccef3
kapaak <pavel.zaplet25@gmail.com>
2022-06-07 18:19:42 +0200

    feat: make projects to display by the latest selected date in sanity

--- pages/index.js
+++ pages/index.js
@@ -24,15 +24,22 @@ export default function Home({ detail, projects }) {
 
 export const getStaticProps = async pageContext => {
 	const query = `*[ _type == "detail"][0]{aboutSection{"file":file.asset->{url},photo,text},educationSection[]{study,location,date},experienceSection[]{company,date,job}}`;
-	const query2 = `*[_type == "project"] `;
+	const query2 = `*[_type == "project"]`;
 
 	const detail = await client.fetch(query);
 	const projects = await client.fetch(query2);
 
+	const sortedProjectsByDate = projects.sort((a, b) => {
+		const dateA = new Date(a.whenCreated);
+		const dateB = new Date(b.whenCreated);
+
+		return dateA - dateB;
+	});
+
 	if (!detail || !projects) return { props: null, notFound: true };
 	else
 		return {
-			props: { detail, projects },
+			props: { detail, projects: sortedProjectsByDate.reverse() },
 			revalidate: 10,
 		};
 };
