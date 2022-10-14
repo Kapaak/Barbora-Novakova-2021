c2e2cfa55167accd6fda3b8f3fc4e39fedc03082
kapaak <pavel.zaplet25@gmail.com>
2022-10-14 23:37:13 +0200

    feat: add google anal

--- pages/_app.js
+++ pages/_app.js
@@ -2,10 +2,23 @@ import Footer from '../components/Footer'
 import Navigation from '../components/Navigation'
 import { GlobalStyles } from '../styles/index'
 import HeadSelector from '../utils/HeadSelector'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){window.dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
+            `}
+      </Script>
       <GlobalStyles />
       <HeadSelector />
       <Navigation />
